f6234ac25251d89012f05ada1dc44f73b3e262a9
Moretto <mark.moretto@forcepoint.com>
2020-12-04 12:39:25 -0600

    added commentary to part 2

--- 2020/day_4.py
+++ 2020/day_4.py
@@ -95,93 +95,44 @@ constraints = {
     "cid": r"(.*?)",
     }
 
-# a = "000000001"
-# b = "0123456789"
-# re.search(r"(\d{9})", a)
 
-# kwrd_ptrn = r"([\w]+):([.]+)\s*?"
-# p = re.compile(kwrd_ptrn)
-
-# t1 = "ecl:#eef340 eyr:2023 hcl:#c0946f pid:244684338 iyr:2020 cid:57 byr:1969 hgt:152cm"
-# p.findall(t1)
-
-# t2 = "iyr:1993 hgt:74cm eyr:1960 byr:2029 hcl:293244 ecl:#3cb5e5 pid:4861232363"
-# p.findall(t2)
-
-# # Invalid sample
-# sample = """
-# eyr:1972 cid:100
-# hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
-
-# iyr:2019
-# hcl:#602927 eyr:1967 hgt:170cm
-# ecl:grn pid:012533040 byr:1946
-
-# hcl:dab227 iyr:2012
-# ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
-
-# hgt:59cm ecl:zzz
-# eyr:2038 hcl:74454a iyr:2023
-# pid:3556412378 byr:2007
-# """.strip()
-
-# # Valid sample
-# sample = """
-# pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
-# hcl:#623a2f
-
-# eyr:2029 ecl:blu cid:129 byr:1989
-# iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-
-# hcl:#888785
-# hgt:164cm byr:2001 iyr:2015 cid:88
-# pid:545766238 ecl:hzl
-# eyr:2022
-
-# iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
-# """.strip()
-
-# lines = [line.strip() for line in sample.splitlines()]
-# pports = list(map(lambda x: str(x).strip(),
-#                   str(" ".join(["`" if len(line) == 0 else line for line in lines]))
-#                   .split("`")))
 
 valid_count = 0
 for pport in pports:
-    
+    # Split passports by whitespace, then by colon
+    # Unpack to id and value variables.
     ids, values = zip(*[re.split(r"\s?:\s?", el.strip()) for el in re.split(r"\s", pport)])
     
-
+    # Find set difference between expected and actual.
+    # Capture missing elements in variable.
     diff = fieldset.difference(set(ids))
 
+    # Difference count between actual and expected should be less than 2.
+    # Difference can be one as long as the value is 'cid'
     if len(diff) < 2:
         
+        # Validate constraints using regular expressions.
+        # Count number of valid values in each passport
         regex_count=0
+        
+        # Create a key-value collection for analysis
         ddict = dict(zip(ids, values))
+        
+        # Iterate dictionary
+        # Compile regex value
+        # Use .search() to determine if value is valid
         for k, v in ddict.items():
             p = re.compile(constraints[k])
             if p.search(v):
                 regex_count += 1
+                
+        # If regex_count is 8, then passport valid
+        # If regex_count is 7 and 'cid' in id list, then passport valid
         if regex_count == 8:
             valid_count += 1
+            
         elif regex_count == 7 and "cid" in diff:
             valid_count += 1
-        
-                
-
-results = dict(passport=[], isvalid=[], ids=[], id_count=[])
-    results["passport"].append(pport)
-    results["isvalid"].append(validated)
-    results["ids"].append(ids)
-    results["id_count"].append(len(ids))
-
-    
-zipres = list(zip(results["passport"], results["isvalid"], results["ids"], results["id_count"]))
-for el in zipres:
-    # if el[1] == False:
-    #     print(el)
-    if el[1] and el[3] < 8:
-        print(el[0], el[2], el[3])
 
 
 if __name__ == "__main__":
