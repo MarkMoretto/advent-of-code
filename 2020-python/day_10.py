94292ef8892fe6ea2630357b4838ab959950f0df
Mark M <otteromkram@gmail.com>
2020-12-10 20:37:14 -0600

    reworked day 10 to reduce some unnecessary script (variables, functions, etc.)

--- 2020-python/day_10.py
+++ 2020-python/day_10.py
@@ -9,20 +9,14 @@ Contributor(s):
     Mark M.
 """
 
-import queue
-# import threading
-import concurrent.futures as ccf
+
 from array import array
-import itertools as ittr
 
 from utils import current_file, day_number, get_lines, read_data
 
 # If testing, set DEBUG to True for a smaller data set.
 DEBUG: bool = True
 
-
-# Allowed gaps between outlet adapter joltage ratings.
-DIFFS: tuple = (1, 2, 3)
 
 
 # Import data
@@ -94,19 +88,18 @@ d_len = len(data)
 ######### --- Part 1 --- ###########
 ####################################
 
+apicks = array("B")
 ones, threes = 0, 1
-picks = [0]
+apicks.append(0)
 for i in range(d_len):
-    curr, nxt = picks[-1], data[i]
+    curr, nxt = apicks[-1], data[i]
     tmp = nxt - curr
     if tmp < 4:
-        picks.append(nxt)
+        apicks.append(nxt)
         if tmp == 1:
             ones += 1
-            # print(f"ones: {curr} {nxt}")
         elif tmp == 3:
             threes += 1
-            # print(f"threes: {curr} {nxt}")
 
 picks.append(max(data) + 3)
 
@@ -119,15 +112,14 @@ print(f"Part 1\n\tOnes: {ones}\n\tThrees: {threes}\n\tOnes * Threes: {ones * thr
 ######### --- Part 2 --- ###########
 ####################################
 
-apicks = array("B")
-apicks.fromlist(picks)
-length = apicks.buffer_info()[1]
-min_pick, max_pick = min(apicks), max(apicks)
+# Allowed gaps between outlet adapter joltage ratings.
+DIFFS: tuple = (1, 2, 3)
 
 
 # Remove frst zero from array
 apicks.pop(0)
 
+# Create dictionary for holding path combos
 paths_dict = {}
 paths_dict[0] = 1
 for adapter in apicks:
@@ -139,97 +131,3 @@ for adapter in apicks:
 
 print(f"Part 2: Distinct N ways to arrange adapters: {max(paths_dict.values())}")
 
-
-
-# ddict = {}
-# for i in range(length-1):
-#     a = picks[i]
-#     ddict[a] = []
-#     for j in range(i+1, length):
-#         b = picks[j]
-#         diff = b - a
-#         if diff < 4:
-#             ddict[a].append(apicks[j])
-
-# least_picks = array("B")
-# target = min(ddict)
-# for k, v in ddict.items():
-#     if k == target:
-#         least_picks.append(k)
-#         target = max(v)
-# least_picks.append(max(apicks))
-
-
-
-
-# def combo_test(arr, arr_len):
-#     res = True
-#     for i in range(arr_len-1):
-#         if arr[i+1] - arr[i] >= 4:
-#             res = False
-#             break
-#     return res
-
-
-# def get_combos(N):
-#     tmp = []
-#     L = length - N
-#     for combo in ittr.combinations(apicks, L):
-#         if combo[0] == min_pick and combo[-1] == max_pick:
-#             if combo_test(combo, L):
-#                 tmp.append(combo)  
-#     return tmp
-
-
-# # def even_combos(n):
-# #     tmp = []
-# #     for i in range(0, n, 2):
-# #         res = get_combos(i)
-# #         tmp.extend(res)
-# #     return tmp
-
-
-# # def odd_combos(n):
-# #     tmp = []
-# #     for i in range(1, n, 2):
-# #         res = get_combos(i)
-# #         tmp.extend(res)
-# #     return tmp
-
-
-# def proc_combos(rng):
-#     return [get_combos(i) for i in rng]
-
-
-
-# start_length = least_picks.buffer_info()[1]
-# # min_pick, max_pick = min(apicks), max(apicks)
-# # start_length = length // 4
-
-
-# num_threads = min(50, length)
-
-# """
-# # Works
-# rng = [length-i for i in range(start_length, length+1)]
-# tpe = ccf.ThreadPoolExecutor(max_workers=num_threads)
-# wait_for = [tpe.submit(get_combos, i) for i in rng]
-# combos = []
-# for f in ccf.as_completed(wait_for):
-#     combos.append(f.result())
-# final = [x for y in combos for x in y]
-# tpe.shutdown()
-# """
-
-
-# rng = [length-i for i in range(start_length, length+1)]
-
-# combos = []
-# with ccf.ThreadPoolExecutor(max_workers=num_threads) as tpe:
-#     wait_for = [tpe.submit(get_combos, i) for i in rng]
-#     for f in ccf.as_completed(wait_for):
-#         combos.append(f.result())
-
-# final = [x for y in combos for x in y]
-
-            
