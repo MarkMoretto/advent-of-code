9e4427c5dda6588be5fe45e1887d9357bbfb565a
Mark M <otteromkram@gmail.com>
2020-12-10 20:31:56 -0600

    day 10 complete

--- 2020-python/day_10.py
+++ 2020-python/day_10.py
@@ -9,10 +9,9 @@ Contributor(s):
     Mark M.
 """
 
-import re
 import queue
-import threading
-import concurrent.futures
+# import threading
+import concurrent.futures as ccf
 from array import array
 import itertools as ittr
 
@@ -60,18 +59,18 @@ if DEBUG:
 10
 3
 """
-    raw_data = """16
-10
-15
-5
-1
-11
-7
-19
-6
-12
-4
-"""
+#     raw_data = """16
+# 10
+# 15
+# 5
+# 1
+# 11
+# 7
+# 19
+# 6
+# 12
+# 4
+# """
 else:
     # Current file filepath
     thisfile = current_file(__file__)
@@ -120,129 +119,117 @@ print(f"Part 1\n\tOnes: {ones}\n\tThrees: {threes}\n\tOnes * Threes: {ones * thr
 ######### --- Part 2 --- ###########
 ####################################
 
-
-ddict = {}
-for i in range(len(picks)):
-    for j in range(i+1, len(picks)):
-        print(picks[i], picks[j])
-    curr, nxt = picks[i-1], picks[i]
-    diff = nxt - curr
-    
-    ddict[i] = []
-    j = i + 1
-    
-
-
-def combo_test(arr, arr_len):
-    res = True
-    for i in range(arr_len-1):
-        if arr[i+1] - arr[i] >= 4:
-            res = False
-            break
-    return res
-
-
-
-def get_combos(N):
-    tmp = []
-    L = length - N
-    for combo in ittr.combinations(apicks, L):
-        if combo[0] == min_pick and combo[-1] == max_pick:
-            if combo_test(combo, L):
-                tmp.append(combo)  
-    return tmp
-
-
-def even_combos(n):
-    tmp = []
-    for i in range(0, n, 2):
-        res = get_combos(i)
-        tmp.extend(res)
-    return tmp
-
-
-def odd_combos(n):
-    tmp = []
-    for i in range(1, n, 2):
-        res = get_combos(i)
-        tmp.extend(res)
-    return tmp
-
-
-def proc_combos(rng):
-    return [get_combos(i) for i in rng]
-
-
 apicks = array("B")
 apicks.fromlist(picks)
-
 length = apicks.buffer_info()[1]
 min_pick, max_pick = min(apicks), max(apicks)
-start_length = length // 4
 
 
-num_threads = min(50, len(apicks))
+# Remove frst zero from array
+apicks.pop(0)
 
-combos = []
-rng = range(start_length, length)
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as tpe:
-    fdict = {tpe.submit(get_combos, i):i for i in rng}
-    for f in concurrent.futures.as_completed(fdict):
-        idx = fdict[f]
-        try:
-            tmp = f.result()
-            combos.append(tmp)
-        except ValueError as ve:
-            print(f"Error for index {idx}: {ve}")
-        
+paths_dict = {}
+paths_dict[0] = 1
+for adapter in apicks:
+    tot_paths = 0
+    for n in DIFFS:
+        tot_paths += paths_dict.get(adapter - n, 0)
+    paths_dict[adapter] = tot_paths
+
+
+print(f"Part 2: Distinct N ways to arrange adapters: {max(paths_dict.values())}")
+
+
+
+# ddict = {}
+# for i in range(length-1):
+#     a = picks[i]
+#     ddict[a] = []
+#     for j in range(i+1, length):
+#         b = picks[j]
+#         diff = b - a
+#         if diff < 4:
+#             ddict[a].append(apicks[j])
+
+# least_picks = array("B")
+# target = min(ddict)
+# for k, v in ddict.items():
+#     if k == target:
+#         least_picks.append(k)
+#         target = max(v)
+# least_picks.append(max(apicks))
+
+
+
+
+# def combo_test(arr, arr_len):
+#     res = True
+#     for i in range(arr_len-1):
+#         if arr[i+1] - arr[i] >= 4:
+#             res = False
+#             break
+#     return res
+
+
+# def get_combos(N):
+#     tmp = []
+#     L = length - N
+#     for combo in ittr.combinations(apicks, L):
+#         if combo[0] == min_pick and combo[-1] == max_pick:
+#             if combo_test(combo, L):
+#                 tmp.append(combo)  
+#     return tmp
+
+
+# # def even_combos(n):
+# #     tmp = []
+# #     for i in range(0, n, 2):
+# #         res = get_combos(i)
+# #         tmp.extend(res)
+# #     return tmp
+
+
+# # def odd_combos(n):
+# #     tmp = []
+# #     for i in range(1, n, 2):
+# #         res = get_combos(i)
+# #         tmp.extend(res)
+# #     return tmp
+
+
+# def proc_combos(rng):
+#     return [get_combos(i) for i in rng]
+
+
+
+# start_length = least_picks.buffer_info()[1]
+# # min_pick, max_pick = min(apicks), max(apicks)
+# # start_length = length // 4
+
+
+# num_threads = min(50, length)
+
+# """
+# # Works
+# rng = [length-i for i in range(start_length, length+1)]
+# tpe = ccf.ThreadPoolExecutor(max_workers=num_threads)
+# wait_for = [tpe.submit(get_combos, i) for i in rng]
+# combos = []
+# for f in ccf.as_completed(wait_for):
+#     combos.append(f.result())
+# final = [x for y in combos for x in y]
+# tpe.shutdown()
+# """
+
+
+# rng = [length-i for i in range(start_length, length+1)]
+
+# combos = []
+# with ccf.ThreadPoolExecutor(max_workers=num_threads) as tpe:
+#     wait_for = [tpe.submit(get_combos, i) for i in rng]
+#     for f in ccf.as_completed(wait_for):
+#         combos.append(f.result())
+
+# final = [x for y in combos for x in y]
+
             
-# even_rng = range(0, h_length, 2)
-# odd_rng = range(1, h_length, 2)
-
-q = queue.Queue(maxsize=0)
-num_theads = min(50, len(apicks))
-
-t_list = []
-
-t1 = threading.Thread(target=lambda x, y: q.put(even_combos(y)), args=(q, length))
-t1.start()
-t_list.append(t1)
-
-t2 = threading.Thread(target=lambda x, y: q.put(odd_combos(y)), args=(q, length))
-t2.start()
-t_list.append(t2)
-
-t3 = threading.Thread(target=lambda x, y: q.put(even_combos(y)), args=(q, length))
-t3.start()
-t_list.append(t3)
-
-t2 = threading.Thread(target=lambda x, y: q.put(odd_combos(y)), args=(q, length))
-t2.start()
-t_list.append(t2)
-
-for t in t_list:
-    t.join()
-
-combos = []
-while not q.empty():
-    combos.append(q.get())
-    
-combos = [x for y in combos for x in y]
-
-# t1 = threading.Thread(target=even_combos, args=(h_length,)) 
-# t2 = threading.Thread(target=odd_combos, args=(h_length,))
-# t1.start()
-# t2.start()
-# res1 = t1.join()
-# res2 = t2.join()
-
-combos = res1 + res2
-
-
-list(range(0, 6, 2)) # 0, 2, 4
-
-list(range(1, 6, 2)) # 1, 3, 5
-
-
-
-
