6045870f2324155c3197808d513c9891c2273228
Mark M <otteromkram@gmail.com>
2020-12-26 16:17:02 -0600

    adding excel, for some reason

--- 2020-python/day_22.py
+++ 2020-python/day_22.py
@@ -8,7 +8,6 @@ Contributor(s):
     Mark M.
 """
 import re
-from itertools import cycle
 from utils import current_file, day_number, get_lines, read_data
 
 
@@ -44,16 +43,12 @@ else:
 # raw_data = read_data(f"day-22-input.txt")
 
 # Split data into lines for further processing.  Skip any missing or blank lines.
-# raw_data = raw_data.replace("\r\n", "\n\n")
 raw_data = raw_data.replace("\r", "")
 raw_data = re.sub(r"Player \d:\n?", "", raw_data)
 
 p1, p2 = raw_data.split("\n\n")
 p1cards = list(map(int, p1.split("\n")))
 p2cards = list(map(int, p2.split("\n")))
-
-# p1cyc = cycle(p1)
-# p2cyc = cycle(p2)
 
 while True:
     p1card = p1cards.pop(0)
@@ -77,6 +72,24 @@ print(f"Part 1: The total point value is: {result}")
 
 
 
+# Part 2
+"""
+Recursive Combat still starts by splitting the cards into two decks (you offer to play with the same starting decks as before - it's only fair). Then, the game consists of a series of rounds with a few changes:
+
+* Before either player deals a card, if there was a previous round in this game that
+had exactly the same cards in the same order in the same players' decks, the game
+instantly ends in a win for player 1. Previous rounds from other games are not
+considered. (This prevents infinite games of Recursive Combat, which everyone agrees
+is a bad idea.)
+* Otherwise, this round's cards must be in a new configuration; the players begin the
+round by each drawing the top card of their deck as normal.
+* If both players have at least as many cards remaining in their deck as the value of
+the card they just drew, the winner of the round is determined by playing a new game
+of Recursive Combat (see below).
+* Otherwise, at least one player must not have enough cards left in their deck to
+recurse; the winner of the round is the player with the higher-value card.
+
+"""
 
 
 
