d4ff64899f541bb1ce0b69b71d71e16f9ac8e844
mark moretto <otteromkram@gmail.com>
2021-12-01 11:58:10 -0600

    completed day 1

---
+++ src/day_01.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+
+'''
+URL: https://adventofcode.com/2021/day/1
+'''
+
+from pathlib import Path
+
+DATA_DIR = Path("data")
+DATA_FILE_A = DATA_DIR.joinpath("day1a.txt")
+data = DATA_FILE_A.open(mode="r", encoding="utf-8").read().splitlines()
+
+depth_data = """199
+200
+208
+210
+200
+207
+240
+269
+260
+263""".strip().splitlines()
+
+# depths = stoi(depth_data)
+
+stoi = lambda string_list: list(map(int, string_list))
+
+def quantify(iterable, pred=bool):
+    return sum(map(pred, iterable))
+
+
+def agg_sum(fterator):
+    return sum(fterator)
+
+def f(predicate: bool, *args):
+    return map(predicate, *args)
+
+
+depths = stoi(data)
+
+part_a_result = agg_sum(
+    f(lambda a, b: 1 if b-a>0 else 0, depths[:-1], depths[1:])
+)
+print(part_a_result)
+
+
+# --- Part 2 --- #
+WINDOW_SIZE = 3
+# sm_depths = stoi(depth_data)
+# max_range = len(sm_depths) - WINDOW_SIZE + 1
+
+
+def solution_b(depth_list: list, stepsize: int = WINDOW_SIZE) -> int:
+    max_range = len(depth_list) - stepsize + 1
+    increased_count = 0
+    prev_measure = 0
+    for i in range(max_range):
+        if i == 0:
+            prev_measure = sum(depth_list[i:i+stepsize])
+        else:
+            current_measure = sum(depth_list[i:i+stepsize])
+            if (current_measure - prev_measure) > 0:
+                increased_count += 1
+            prev_measure = current_measure
+    return increased_count
+
+# solution_b(sm_depths) # 5
+part_b_result = solution_b(depths)
+print(part_b_result)
