63977fe28a6fee6e6bd4a9430f1c773100832e35
mark moretto <otteromkram@gmail.com>
2021-12-02 17:48:10 -0600

    think day 1 might be close

---
+++ golang/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	d "aoc2021/day1"
+	"fmt"
+	"os"
+	"bufio"
+	"strconv"
+)
+
+var dataFileName string = "day1.txt"
+
+const dataFolder string = "data"
+var FilePath string
+var pFilePath *string = &FilePath
+
+func init() {
+	*pFilePath = fmt.Sprintf("%s/%s", dataFolder, dataFileName)
+}
+
+func getData(output *[]int) {
+
+	file, err := os.Open(FilePath)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+		i, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			fmt.Println(err)
+		}
+		*output = append(*output, i)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}	
+}
+
+func main() {
+	var dataMain []int
+	getData(&dataMain)
+	d.Solution(dataMain)
+}
