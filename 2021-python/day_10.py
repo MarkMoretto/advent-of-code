7b5c8b0e0b4a8aae60eccaac5b7a86a70ef6de88
mark moretto <otteromkram@gmail.com>
2021-12-10 15:16:06 -0600

    Completed AOC 2021 - Day 10 using Python 3.10

--- 2021-python/day_10.py
+++ 2021-python/day_10.py
@@ -25,9 +25,9 @@ Integer = int
 Number = Union[int, float]
 NumList = List[Number]
 
-# Get data
+# --- Get data --- #
 AOC_DAY: int = 10
-USE_SAMPLE_TF: bool = True
+USE_SAMPLE_TF: bool = False
 
 DATA_FILE_A = DATA_DIR.joinpath(
     f"day{AOC_DAY:0>2}{'-sample' if USE_SAMPLE_TF else ''}.txt"
@@ -36,30 +36,26 @@ raw_data = DATA_FILE_A.open(mode="r", encoding="utf-8").read()
 
 lines = raw_data.splitlines()
 
-
-def delete_nth(d, n):
-    """Delete nth item from deque."""
-    d.rotate(-n)
-    _ = d.popleft()
-    d.rotate(n)
-
+# --- Helper functions
 def view(d: deque) -> None:
+    """Print enumerated deque elements."""
     print("\n".join(f"{i:<4}{v}" for i, v in enumerate(d)))
 
 def spacy(iterable) -> None:
+    """Print single-whitespace separated elements."""
     print(" ".join(list(iterable)))
 
 def pprint(d: dict) -> None:
+    """Print dictionary items to individual lines."""
     print("\n".join(map(lambda k,v: f"{k} : {v}", d.keys(), d.values())))
 
 
-bracket_str = r"<> {} [] ()"
-bracket_map = {b[0]:b[1] for b in bracket_str.split()}
+# Bracket pairs and maps.
+bracket_pairs = r"<> {} [] ()".split()
+bracket_map = {b[0]:b[1] for b in bracket_pairs}
+inv_bracket_map = {v:k for k, v in bracket_map.items()}
 open_bracks = set(bracket_map.keys())
 close_bracks = set(bracket_map.values())
-
-
-# line_lengths = {i:len(line) for i, line in enumerate(lines)}
 
 
 ##################
@@ -102,6 +98,59 @@ print(f"Part 1 solution: {result}")
 # --- Part 2 --- #
 ##################
 
+# Get remaining list items.
 lines_2 = [line for i, line in enumerate(lines) if not i in corrupted_lines]
 
+# Updated value map for Part 2
+value_map = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
+
+def calc_score(missing: str, score_map: dict = value_map) -> int:
+    """Returns cumulative score for all missing items."""
+    scr = 0
+    for m in missing:
+        scr *= 5
+        scr += score_map[m]
+    return scr
+
+def get_middle_score(items: list) -> int:
+    """Return middle score from list of scores.  Total item count
+    for list should be odd.
+    """
+    middle = (len(items) // 2)
+    return items[middle]
+
+# Hold results for each line.
+results = list()
+for line in lines_2:
+    # We will continue replacing all valid bracket pairs
+    # untill there are no more replacements to be made.
+    while True:
+        if not any(b in line for b in bracket_pairs):
+            break
+        else:
+            for b in bracket_pairs:
+                if b in line:
+                    line = line.replace(b, "")
+
+    # Reverse the list and concatenate into a string.
+    missing_brackets = "".join([bracket_map[b] for b in line[::-1]])
+
+    # Append key-map for scores and missing brackets to results list.
+    results.append({
+        "missing_brackets": missing_brackets,
+        "score": calc_score(missing_brackets)
+    })
+
+# Retrieve all scores and sort
+scores = [r["score"] for r in results]
+scores.sort(reverse=True)
+
+# Find middle value as result and send to stdout.
+result = get_middle_score(scores)
+print(f"Part 2 solution: {result}")
 
